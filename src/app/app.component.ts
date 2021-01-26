a2daff757d680cfdf5d38ca59bd4d19cfb06450f
GAHYUN LEE <gahyunlee26@gmail.com>
2021-01-26 13:53:52 -0800

    service worker check updates in angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,14 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'serviceworker-angular';
   apiData: any;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private update: SwUpdate,
+    private appRef: ApplicationRef
+  ) {
+    this.updateClient();
+    this.checkUpdate();
+  }
 
   ngOnInit() {
     this.http.get(`http://dummy.restapiexample.com/api/v1/employees`).subscribe(
@@ -20,5 +29,35 @@ export class AppComponent {
         console.error(err);
       }
     );
+  }
+
+  updateClient() {
+    if (!this.update.isEnabled) {
+      console.log('Not Enabled');
+      return;
+    }
+    this.update.available.subscribe((event) => {
+      console.log(`current`, event.current, `available `, event.available);
+      if (confirm('update available for the app please conform')) {
+        this.update.activateUpdate().then(() => location.reload());
+      }
+    });
+
+    this.update.activated.subscribe((event) => {
+      console.log(`current`, event.previous, `available `, event.current);
+    });
+  }
+
+  checkUpdate() {
+    this.appRef.isStable.subscribe((isStable) => {
+      if (isStable) {
+        const timeInterval = interval(15000);
+
+        timeInterval.subscribe(() => {
+          this.update.checkForUpdate().then(() => console.log('checked'));
+          console.log('update checked');
+        });
+      }
+    });
   }
 }
